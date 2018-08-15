fe9e7ecb48fd449dc62a0fb8da99066fa5d71240
Du Liu <duliu@Dus-MacBook-Pro.local>
2018-08-15 23:15:30 +0800

    add banner demo

---
+++ spring-boot-banner-demo/src/main/java/com/devnp/springbootbannerdemo/SpringBootBannerDemoApplication.java
@@ -0,0 +1,12 @@
+package com.devnp.springbootbannerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBannerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBannerDemoApplication.class, args);
+	}
+}
---
+++ spring-boot-banner-demo/src/test/java/com/devnp/springbootbannerdemo/SpringBootBannerDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.devnp.springbootbannerdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootBannerDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
